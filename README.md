e569e2e47cdd7478291aa172e09afedf694543c6
Milos Gajdos <milosgajdos83@gmail.com>
2018-11-23 13:49:22 +0000

    First commit. We can build binary both locally and remotely in Azure.

---
+++ README.md
@@ -0,0 +1,176 @@
+# Restricted Zone Notifier
+
+| Details            |              |
+|-----------------------|---------------|
+| Target OS:            |  Ubuntu\* 16.04 LTS   |
+| Programming Language: |  Go |
+| Time to Complete:    |  45 min     |
+
+![app image](./images/restricted-zone-notifier.png)
+
+## Introduction
+
+This restricted zone notifier application is one of a series of reference implementations for Computer Vision (CV) using the OpenVINO™ toolkit. This application is designed for a machine mounted camera system that monitors if there are any humans present in a predefined selected assembly line area. It sends an alert if there is at least one person detected in the marked assembly area. The user can select the area coordinates either via command line parameters or once the application has been started they can select the region of interest (ROI) by pressing `ESC` key; this will pause the application, pop up a separate window on which the user can drag the mouse from the upper left ROI corner to whatever the size they require the area to cover. By default the whole frame is selected.
+
+This example is intended to demonstrate how to use CV to improve assembly line safety for human operators and factory workers.
+
+## Requirements
+
+### Hardware
+* 6th Generation Intel® Core™ processor with Intel® Iris® Pro graphics and Intel® HD Graphics
+
+### Software
+* [Ubuntu\* 16.04 LTS](http://releases.ubuntu.com/16.04/)
+*Note*: You must be running kernel version 4.7+ to use this software. We recommend using a 4.14+ kernel to use this software. Run the following command to determine your kernel version:
+```
+uname -a
+```
+* OpenCL™ Runtime Package
+* OpenVINO™ toolkit
+
+## Setup
+
+### Install OpenVINO™ Toolkit
+Refer to https://software.intel.com/en-us/articles/OpenVINO-Install-Linux for more information about how to install and setup the OpenVINO™ toolkit.
+
+You will need the OpenCL™ Runtime package if you plan to run inference on the GPU as shown by the
+instructions below. It is not mandatory for CPU inference.
+
+## How it Works
+
+The application uses a video source, such as a camera, to grab frames, and then uses a Deep Neural Network (DNNs) to process the data. The network detects persons in the frame, and then if successful it checks if the detected persons are in the indicated off-limits assembly line region.
+
+The data can then optionally be sent to a MQTT machine to machine messaging server, as part of an industrial data analytics system.
+
+The DNN model used in this application is an Intel® optimized model that is part of the OpenVINO™ toolkit.
+
+You can find it here:
+
+- `/opt/intel/computer_vision_sdk/deployment_tools/intel_models/pedestrian-detection-adas-0002`
+
+![Code organization](./images/arch3.png)
+
+The program creates three threads for concurrency:
+
+- Main goroutine that performs the video i/o
+- Worker goroutine that processes video frames using the deep neural networks
+- Worker goroutine that publishes MQTT messages to remote server
+
+## Setting the Build Environment
+
+You must configure the environment to use the OpenVINO™ toolkit one time per session by running the following command:
+```
+    source /opt/intel/computer_vision_sdk/bin/setupvars.sh
+```
+
+## Building the Code
+
+Start by changing the current directory to wherever you have git cloned the application code. For example:
+```
+    cd restricted-zone-notifier-go
+```
+
+Before you can build the program you need to fetch its dependencies. You can do that by running the commands below. The first one fetches `Go` depedency manager of our choice and the latter uses it to satisfy the program's depdencies as defined in `Gopkg.lock` file:
+
+```
+make godep
+make dep
+```
+
+Once you have fetched the dependencies you must export a few environment variables required to build the library from the fetched dependencies. Run the following command from the project directory:
+
+```
+    source vendor/gocv.io/x/gocv/openvino/env.sh
+```
+
+Now you are ready to build the program binary. The project ships a simple `Makefile` which makes building the program easy by invoking the `build` task from the project root as follows:
+```
+    make build
+```
+
+ This commands creates a new directory called `build` in your current working directory and places the newly built binary called `monitor` into it.
+Once the commands are finished, you should have built the `monitor` application executable.
+
+## Running the Code
+
+To see a list of the various options:
+```
+    cd build
+    ./monitor -help
+```
+
+To run the application with the needed model using the webcam:
+```
+    ./monitor -model=/opt/intel/computer_vision_sdk/deployment_tools/intel_models/pedestrian-detection-adas-0002/FP32/pedestrian-detection-adas-0002.bin -model-config=/opt/intel/computer_vision_sdk/deployment_tools/intel_models/pedestrian-detection-adas-0002/FP32/pedestrian-detection-adas-0002.xml
+```
+
+You can select an area to be used as the "off-limits" area by pressing the `c` key once the program is running. A new window will open showing a still image from the video capture device. Drag the mouse from left top corner to cover an area on the plane and once done (a blue rectangle is drawn) present `ENTER` or `SPACE` to proceed with monitoring.
+
+Once you have selected the "off-limits" area the coordinates will be displayed in the terminal window like this:
+```
+Assembly Area Selection: -x=429 -y=101 -height=619 -width=690
+```
+
+You can run the application using those coordinates by using the `-x`, `-y`, `-height, -h`, and `-width, -w` flags to pre-select that area.
+
+For example:
+```
+    ./monitor -model=/opt/intel/computer_vision_sdk/deployment_tools/intel_models/pedestrian-detection-adas-0002/FP32/pedestrian-detection-adas-0002.bin -model-config=/opt/intel/computer_vision_sdk/deployment_tools/intel_models/pedestrian-detection-adas-0002/FP32/pedestrian-detection-adas-0002.xml -x=429 -y=101 -height=619 -width=690
+```
+
+If you do not select or specify an area, the default is to use the entire window as the off limits area.
+
+### Hardware Acceleration
+
+This application can take advantage of the hardware acceleration in the OpenVINO toolkit by using the `-backend, -b` and `-target, -t` parameters.
+
+For example, to use the OpenVINO™ toolkit backend with the GPU in 32-bit mode:
+```
+    ./monitor -model=/opt/intel/computer_vision_sdk/deployment_tools/intel_models/pedestrian-detection-adas-0002/FP32/pedestrian-detection-adas-0002.bin -model-config=/opt/intel/computer_vision_sdk/deployment_tools/intel_models/pedestrian-detection-adas-0002/FP32/pedestrian-detection-adas-0002.xml -backedn=2 -target=1
+```
+
+To run the code using 16-bit floats, you have to both set the `-target` flag to use the GPU in 16-bit mode, as well as use the FP16 version of the Intel® models:
+```
+    ./monitor -model=/opt/intel/computer_vision_sdk/deployment_tools/intel_models/pedestrian-detection-adas-0002/FP32/pedestrian-detection-adas-0002.bin -model-config=/opt/intel/computer_vision_sdk/deployment_tools/intel_models/pedestrian-detection-adas-0002/FP32/pedestrian-detection-adas-0002.xml -backend=2 -target=2
+```
+
+To run the code using the VPU, you have to set the `-target` flag to `3` and also use the 16-bit FP16 version of the Intel® models:
+```
+    ./monitor -model=/opt/intel/computer_vision_sdk/deployment_tools/intel_models/pedestrian-detection-adas-0002/FP32/pedestrian-detection-adas-0002.bin -model-config=/opt/intel/computer_vision_sdk/deployment_tools/intel_models/pedestrian-detection-adas-0002/FP32/pedestrian-detection-adas-0002.xml -backend=2 -target=3
+```
+
+## Sample Videos
+
+There are several videos available to use as sample videos to show the capabilities of this application. You can download them by running these commands from the `restricted-zone-notifier-cpp` directory:
+```
+    mkdir resources
+    cd resources
+    wget https://github.com/intel-iot-devkit/sample-videos/raw/master/worker-zone-detection.mp4
+    cd ..
+```
+
+To then execute the code using one of these sample videos, run the following commands from the `restricted-zone-notifier-cpp` directory:
+```
+    cd build
+    ./monitor -model=/opt/intel/computer_vision_sdk/deployment_tools/intel_models/pedestrian-detection-adas-0002/FP32/pedestrian-detection-adas-0002.bin -model-config=/opt/intel/computer_vision_sdk/deployment_tools/intel_models/pedestrian-detection-adas-0002/FP32/pedestrian-detection-adas-0002.xml -input=../resources/worker-zone-detection.mp4
+```
+
+### Machine to Machine Messaging with MQTT
+
+If you wish to use a MQTT server to publish data, you should set the following environment variables before running the program and use `-publish` flag when launching the program:
+```
+    export MQTT_SERVER=localhost:1883
+    export MQTT_CLIENT_ID=cvservice
+```
+
+Change the `MQTT_SERVER` to a value that matches the MQTT server you are connecting to.
+
+You should change the `MQTT_CLIENT_ID` to a unique value for each monitoring station, so you can track the data for individual locations. For example:
+```
+    export MQTT_CLIENT_ID=zone1337
+```
+
+If you want to monitor the MQTT messages sent to your local server, and you have the `mosquitto` client utilities installed, you can run the following command:
+```
+mosquitto_sub -t 'machine/zone'
+```
---
+++ main.go
@@ -0,0 +1,445 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"image"
+	"image/color"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	"gocv.io/x/gocv"
+)
+
+const (
+	// name is a program name
+	name = "restricted-zone-notifier"
+	// topic is MQTT topic
+	topic = "machine/zone"
+	// alert contains alert message which will get displayed in main window
+	alert = "HUMAN IN RESTRICTED ZONE: PAUSE THE MACHINE!"
+)
+
+var (
+	// deviceID is camera device ID
+	deviceID int
+	// input is path to image or video file
+	input string
+	// model is path to .bin file of pedestrian detection model
+	model string
+	// modelConfig is path to .xml file of pedestrian detection model configuration
+	modelConfig string
+	// modelConfidence is confidence threshold for pedestrian detection model
+	modelConfidence float64
+	// pointX is X coordinate of the top left point of restricted zone on camera feed
+	pointX int
+	// pointY coordinate of the top left point of restricted zone on camera feed
+	pointY int
+	// width is width of the restricted zone in pixels
+	width int
+	// height is height of the restricted zone in pixels
+	height int
+	// backend is inference backend
+	backend int
+	// target is inference target
+	target int
+	// publish is a flag which instructs the program to publish data analytics
+	publish bool
+	// rate is number of seconds between analytics are collected and sent to a remote server
+	rate int
+)
+
+func init() {
+	flag.IntVar(&deviceID, "device", -1, "Camera device ID")
+	flag.StringVar(&input, "input", "", "Path to image or video file")
+	flag.StringVar(&model, "model", "", "Path to .bin file of pedestrian detection model")
+	flag.StringVar(&modelConfig, "model-config", "", "Path to .xml file of model configuration")
+	flag.Float64Var(&modelConfidence, "model-confidence", 0.5, "Confidence threshold for pedestrian detection")
+	flag.IntVar(&target, "target", 0, "Target device. 0: CPU, 1: OpenCL, 2: OpenCL half precision, 3: VPU")
+	flag.IntVar(&backend, "backend", 0, "Inference backend. 0: Auto, 1: Halide language, 2: Intel DL Inference Engine")
+	flag.IntVar(&pointX, "x", 0, "X coordinate of the top left point of restricted zone in camera feed")
+	flag.IntVar(&pointY, "y", 0, "Y coordinate of the top left point of restricted zone in camera feed")
+	flag.IntVar(&width, "width", 0, "Width of the restricted zone in pixels")
+	flag.IntVar(&height, "height", 0, "Height of the restricted zone in pixels")
+	flag.BoolVar(&publish, "publish", false, "Publish data analytics to a remote server")
+	flag.IntVar(&rate, "rate", 1, "Number of seconds between analytics are sent to a remote server")
+}
+
+// Perf stores inference engine performance info
+type Perf struct {
+	// Net stores pedestrian detector performance info
+	Net float64
+}
+
+// String implements fmt.Stringer interface for Perf
+func (p *Perf) String() string {
+	return fmt.Sprintf("Inference time: %.2f ms", p.Net)
+}
+
+// Result is computation result of the monitor returned to main goroutine
+type Result struct {
+	// Alert is used to raise an alert based on the presence of pedestrians in restricted area
+	Alert bool
+}
+
+// String implements fmt.Stringer interface for Result
+func (r *Result) String() string {
+	return fmt.Sprintf("Safe %v", !r.Alert)
+}
+
+// ToMQTTMessage turns result into MQTT message which can be published to MQTT broker
+func (r *Result) ToMQTTMessage() string {
+	return fmt.Sprintf("{\"Safe\" : %v}", !r.Alert)
+}
+
+// getPerformanceInfo queries the Inference Engine performance info and returns it as string
+func getPerformanceInfo(net *gocv.Net) *Perf {
+	freq := gocv.GetTickFrequency() / 1000
+
+	perf := net.GetPerfProfile() / freq
+
+	return &Perf{
+		Net: perf,
+	}
+}
+
+// messageRunner reads data published to pubChan with rate frequency and sends them to remote analytics server
+// doneChan is used to receive a signal from the main goroutine to notify the routine to stop and return
+func messageRunner(doneChan <-chan struct{}, pubChan <-chan *Result, c *MQTTClient, topic string, rate int) error {
+	ticker := time.NewTicker(time.Duration(rate) * time.Second)
+
+	for {
+		select {
+		case <-ticker.C:
+			result := <-pubChan
+			_, err := c.Publish(topic, result.ToMQTTMessage())
+			// TODO: decide whether to return with error and stop program;
+			// For now we just signal there was an error and carry on
+			if err != nil {
+				fmt.Printf("Error publishing message to %s: %v", topic, err)
+			}
+		case <-pubChan:
+			// we discard messages in between ticker times
+		case <-doneChan:
+			fmt.Printf("Stopping messageRunner: received stop sginal\n")
+			return nil
+		}
+	}
+
+	return nil
+}
+
+// detectMotion detects pedstrian motion in restricted zone area and returns alert if the motion has been detected
+func detectMotion(img *gocv.Mat, persons []image.Rectangle, area *image.Rectangle) bool {
+	// we assume there is no motion
+	alert := false
+
+	for i := range persons {
+		if !persons[i].In(image.Rect(0, 0, img.Cols(), img.Rows())) {
+			continue
+		}
+
+		if persons[i].In(*area) {
+			return true
+		}
+	}
+
+	return alert
+}
+
+// detectPersons detects pedestrians in img and returns them as a slice of rectangles that encapsulates them
+func detectPersons(net *gocv.Net, img *gocv.Mat) []image.Rectangle {
+	// convert img Mat to 672x384 blob that the face detector can analyze
+	blob := gocv.BlobFromImage(*img, 1.0, image.Pt(672, 384), gocv.NewScalar(0, 0, 0, 0), false, false)
+	defer blob.Close()
+
+	// run a forward pass through the network
+	net.SetInput(blob, "")
+	results := net.Forward("")
+	defer results.Close()
+
+	// iterate through all detections and append results to persons buffer
+	var persons []image.Rectangle
+	for i := 0; i < results.Total(); i += 7 {
+		confidence := results.GetFloatAt(0, i+2)
+		if float64(confidence) > modelConfidence {
+			left := int(results.GetFloatAt(0, i+3) * float32(img.Cols()))
+			top := int(results.GetFloatAt(0, i+4) * float32(img.Rows()))
+			right := int(results.GetFloatAt(0, i+5) * float32(img.Cols()))
+			bottom := int(results.GetFloatAt(0, i+6) * float32(img.Rows()))
+			persons = append(persons, image.Rect(left, top, right, bottom))
+		}
+	}
+
+	return persons
+}
+
+// frameRunner reads image frames from framesChan and performs face and sentiment detections on them
+// doneChan is used to receive a signal from the main goroutine to notify frameRunner to stop and return
+func frameRunner(framesChan <-chan *gocv.Mat, doneChan <-chan struct{}, resultsChan chan<- *Result,
+	perfChan chan<- *Perf, pubChan chan<- *Result, net *gocv.Net, area *image.Rectangle) error {
+
+	for {
+		select {
+		case <-doneChan:
+			fmt.Printf("Stopping frameRunner: received stop sginal\n")
+			return nil
+		case frame := <-framesChan:
+			// let's make a copy of the original
+			img := gocv.NewMat()
+			frame.CopyTo(&img)
+
+			persons := detectPersons(net, &img)
+
+			alert := detectMotion(&img, persons, area)
+
+			// detection result
+			result := &Result{
+				Alert: alert,
+			}
+
+			// send data down the channels
+			perfChan <- getPerformanceInfo(net)
+			resultsChan <- result
+			if pubChan != nil {
+				pubChan <- result
+			}
+
+			// close image matrices
+			img.Close()
+		}
+	}
+
+	return nil
+}
+
+func parseCliFlags() error {
+	// parse cli flags
+	flag.Parse()
+
+	// path to pedestrian detection model can't be empty
+	if model == "" {
+		return fmt.Errorf("Invalid path to .bin file of pedestrian detection model: %s", model)
+	}
+	// path to pedestrian detection model config can't be empty
+	if modelConfig == "" {
+		return fmt.Errorf("Invalid path to .xml file of pedestrian model configuration: %s", modelConfig)
+	}
+
+	return nil
+}
+
+// NewInferModel reads DNN model and it configuration, sets its preferable target and backend and returns it.
+// It returns error if either the model files failed to be read or setting the target fails
+func NewInferModel(model, config string, backend, target int) (*gocv.Net, error) {
+	// read in Face model and set the target
+	m := gocv.ReadNet(model, config)
+
+	if err := m.SetPreferableBackend(gocv.NetBackendType(backend)); err != nil {
+		return nil, err
+	}
+
+	if err := m.SetPreferableTarget(gocv.NetTargetType(target)); err != nil {
+		return nil, err
+	}
+
+	return &m, nil
+}
+
+// NewCapture creates new video capture from input or camera backend if input is empty and returns it.
+// It fails with error if it either can't open the input video file or the video device
+func NewCapture(input string, deviceID int) (*gocv.VideoCapture, error) {
+	if input != "" {
+		// open video file
+		vc, err := gocv.VideoCaptureFile(input)
+		if err != nil {
+			return nil, err
+		}
+
+		return vc, nil
+	}
+
+	// open camera device
+	vc, err := gocv.VideoCaptureDevice(deviceID)
+	if err != nil {
+		return nil, err
+	}
+
+	return vc, nil
+}
+
+// NewMQTTPublisher creates new MQTT client which collects analytics data and publishes them to remote MQTT server.
+// It attempts to make a connection to the remote server and if successful it return the client handler
+// It returns error if either the connection to the remote server failed or if the client config is invalid.
+func NewMQTTPublisher() (*MQTTClient, error) {
+	// create MQTT client and connect to MQTT server
+	opts, err := MQTTClientOptions()
+	if err != nil {
+		return nil, err
+	}
+
+	// create MQTT client ad connect to remote server
+	c, err := MQTTConnect(opts)
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+// RestrictedZone create a rectangle which encompasses restricted zone are in the image feed and returns it.
+// If negative point coordinates are given, the area will default to the beginning of the frame.
+// If either default areansize values are given (0,0) or negative ones we returned are defaults to the whole frame
+func RestrictedZone(pointX, pointY, width, height, rows, cols int) image.Rectangle {
+	x, y := 0, 0
+	w, h := width, height
+
+	// if negative number given, we will default to the start of the frame
+	if pointX > 0 && pointY > 0 {
+		x = pointX
+		y = pointY
+	}
+
+	// if either default values are given or negative we will default to the whole frame
+	if width <= 0 {
+		w = cols
+	}
+
+	if h <= 0 {
+		h = rows
+	}
+
+	return image.Rect(x, y, x+w, y+h)
+}
+
+func main() {
+	// parse cli flags
+	if err := parseCliFlags(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error parsing command line parameters: %v\n", err)
+		os.Exit(1)
+	}
+
+	// read in pedestrian detection model and set its inference backend and target
+	net, err := NewInferModel(model, modelConfig, backend, target)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating pedestrian detection model: %v\n", err)
+		os.Exit(1)
+	}
+
+	// create new video capture
+	vc, err := NewCapture(input, deviceID)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating new video capture: %v\n", err)
+		os.Exit(1)
+	}
+	defer vc.Close()
+
+	// frames channel provides the source of images to process
+	framesChan := make(chan *gocv.Mat, 1)
+	// errChan is a channel used to capture program errors
+	errChan := make(chan error, 2)
+	// doneChan is used to signal goroutines they need to stop
+	doneChan := make(chan struct{})
+	// resultsChan is used for detection distribution
+	resultsChan := make(chan *Result, 1)
+	// perfChan is used for collecting performance stats
+	perfChan := make(chan *Perf, 1)
+	// sigChan is used as a handler to stop all the goroutines
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, os.Kill, syscall.SIGTERM)
+	// pubChan is used for publishing data analytics stats
+	var pubChan chan *Result
+	// waitgroup to synchronise all goroutines
+	var wg sync.WaitGroup
+
+	if publish {
+		pub, err := NewMQTTPublisher()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to create MQTT publisher: %v\n", err)
+			os.Exit(1)
+		}
+		pubChan = make(chan *Result, 1)
+		// start MQTT worker goroutine
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			errChan <- messageRunner(doneChan, pubChan, pub, topic, rate)
+		}()
+		defer pub.Disconnect(100)
+	}
+
+	// TODO: figure out how to notify  goroutine about area change
+	// rectangle marking restricted area zone
+	area := RestrictedZone(pointX, pointY, width, height, 0, 0)
+	// start frameRunner goroutine
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		errChan <- frameRunner(framesChan, doneChan, resultsChan, perfChan, pubChan, net, &area)
+	}()
+
+	// open display window
+	window := gocv.NewWindow(name)
+	window.SetWindowProperty(gocv.WindowPropertyFullscreen, gocv.WindowAutosize)
+	defer window.Close()
+
+	// prepare input image matrix
+	img := gocv.NewMat()
+	defer img.Close()
+
+	// initialize the result pointers
+	result := new(Result)
+	perf := new(Perf)
+
+monitor:
+	for {
+		if ok := vc.Read(&img); !ok {
+			fmt.Printf("Cannot read image source %v\n", deviceID)
+			break
+		}
+		if img.Empty() {
+			continue
+		}
+
+		framesChan <- &img
+
+		select {
+		case sig := <-sigChan:
+			fmt.Printf("Shutting down. Got signal: %s\n", sig)
+			break monitor
+		case err = <-errChan:
+			fmt.Printf("Shutting down. Encountered error: %s\n", err)
+			break monitor
+		case result = <-resultsChan:
+			perf = <-perfChan
+		default:
+			// do nothing; just display latest results
+		}
+		// TODO: figure out how to notify  goroutine about area change
+		// inference performance and print it
+		gocv.PutText(&img, fmt.Sprintf("%s", perf), image.Point{0, 15},
+			gocv.FontHersheySimplex, 0.5, color.RGBA{0, 0, 0, 0}, 2)
+		// inference results label
+		gocv.PutText(&img, fmt.Sprintf("%s", result), image.Point{0, 40},
+			gocv.FontHersheySimplex, 0.5, color.RGBA{0, 0, 0, 0}, 2)
+		// display alert message when humane enters restricted zone
+		if result.Alert {
+			gocv.PutText(&img, alert, image.Point{0, 80},
+				gocv.FontHersheySimplex, 0.5, color.RGBA{255, 0, 0, 0}, 2)
+		}
+		// show the image in the window, and wait 1 millisecond
+		window.IMShow(img)
+
+		// exit when ESC key is pressed
+		if window.WaitKey(1) == 27 {
+			break monitor
+		}
+	}
+	// signal all goroutines to finish
+	close(doneChan)
+	// wait for all goroutines to finish
+	wg.Wait()
+}
